1925af51808b4e4c007b7143bd28ba3caaf0a373
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2019-07-22 12:57:32 +0300

    cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mailwizz-python-sdk',
-    version='1.0.0',
+    version='1.0.5',
     packages=setuptools.find_packages(),
     url='https://www.mailwizz.com',
     license='MIT',
@@ -18,5 +18,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'multidimensional-urlencode>=0.0.4',
+        'requests>=2.22.0',
+        'urllib3>=1.25.3',
     ],
 )
