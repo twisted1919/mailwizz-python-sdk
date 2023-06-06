8fd96eb3955fe052065bab9ec814f87a63fe00e2
ghimes <ghimescosmin@gmail.com>
2023-06-06 11:21:34 +0300

    new v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mailwizz-python-sdk',
-    version='1.0.0',
+    version='1.0.1',
     packages=setuptools.find_packages(),
     url='https://www.mailwizz.com',
     license='MIT',
